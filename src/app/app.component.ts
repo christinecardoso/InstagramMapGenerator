a485bb252f2193af99e56249b9d25a917b04757f
Marla Narazani <marla.narazani@ibm.com>
2017-07-26 23:44:58 +0200

    added real data rendering back to map /first prototype finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import {GeonameService} from './Geoname.service';
 import {Media} from './Media';
 import {Location} from './Location';
 import {Country} from './Country';
+declare var transmitData: any;
 
 @Component({
   selector: 'app-root',
@@ -13,24 +14,34 @@ import {Country} from './Country';
 })
 export class AppComponent {
   media: Media[] = [];
+  data: String[][] = [];
+
   constructor(
     private instagramService: InstagramService,
     private googleService: GoogleService,
-    private geonameService: GeonameService
+    private geonameService: GeonameService,
   ) {}
+
+  renderData() {
+    console.log(this.data);
+    transmitData(this.data);
+  }
 
   getInstagramMedia() {
     this.instagramService.getInstagramMedia().then(media => {
       this.media = media;
       for (let i = 0; i < this.media.length; i++) {
-        if(this.media[i].location !== null){
-          let location = this.media[i].location;
-          this.geonameService.getCountry(location.latitude,location.longitude).then(data => {
-            let country = data;
-            console.log(country.countryCode);
+        if (this.media[i].location !== null) {
+          const location = this.media[i].location;
+          this.geonameService.getCountry(location.latitude, location.longitude).then(data => {
+            const country = data;
+            const temp: String[] = [];
+            temp.push(country.countryCode);
+            this.data.push(temp);
           });
         }
       }
+      this.renderData();
     });
   }
 }
